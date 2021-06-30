6e69b154932348d06e657432e4148d68c4d015bb
Courtney Frey <cfrey@launchcode.org>
2021-06-30 11:36:39 -0500

    first commit

---
+++ comms-log-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace comms_log_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
