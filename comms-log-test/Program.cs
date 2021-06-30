c35ca469f84c0213ff148c54db24400cafca9d15
Courtney Frey <cfrey@launchcode.org>
2021-06-30 13:16:01 -0500

    added Willow's note

--- comms-log-test/Program.cs
+++ comms-log-test/Program.cs
@@ -7,6 +7,7 @@ namespace comms_log_test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Willow says, 'Hi'.");
         }
     }
 }
